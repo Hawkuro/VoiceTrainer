113f23c28765c0a881aebadc0fe04f8902efa6d1
Hawkuro <hawkuro@gmail.com>
2014-06-04 14:57:55 +0000

    AnalyserNode replaced with ScriptprocessorNode, which calls requestAnimationframe to render

--- VoiceTrainer.js
+++ VoiceTrainer.js
@@ -8,6 +8,8 @@ var analyser;
 var fft;
 var pit;
 var wf;
+var SAMPLE_SIZE = 4096; // MUST be power of 2
+var SAMPLE_RATE = 44100;
 
 window.onload = function() {
 	//Turn gain input into slider
@@ -46,10 +48,10 @@ window.onload = function() {
 
 	function processStream(stream){
 
-    	var osc = new Oscillator(DSP.SINE, 440, 1, 176400, 44100);
+    	var osc = new Oscillator(DSP.SINE, 440, 1, 176400, SAMPLE_RATE);
     	osc.generate();
     	var signal = osc.signal;
-    	var oscBuff = context.createBuffer(1,176400,44100);
+    	var oscBuff = context.createBuffer(1,176400,SAMPLE_RATE);
     	oscBuff.getChannelData(0).set(signal);
     	var oscNode = context.createBufferSource();
     	oscNode.buffer = oscBuff;
@@ -64,12 +66,12 @@ window.onload = function() {
 		filter*/ //microphone.connect(context.destination);
 
 
-		analyser = context.createAnalyser();
+		/*analyser = context.createAnalyser();
 		if(!analyser){
 			alert('Browser does not support AnalyserNode, an imperative tool for this application.');
 			return;
 		}
-		analyser.fftSize=2048;
+		analyser.fftSize=2048;*/
 
 		var gain = context.createGain();
 		gain.gain.value = 1;
@@ -78,9 +80,61 @@ window.onload = function() {
 			//console.log(gain.gain.value);
 		});
 
+
+
+		//For debugging
+		/*console.log(microphone);
+		console.log(analyser);
+		var fbc_array= new Uint8Array(analyser.fftSize); //Not actualy used as an FCB array anymore, but oh well
+		analyser.getByteTimeDomainData(fbc_array);
+		console.log(fbc_array);*/
+
+		//Get some variable ready for the waveform-canvas
+		wf = initCanvas('render-waveform');
+
+		wf.l = SAMPLE_SIZE;//analyser.fftSize;
+		//wf.array = new Uint8Array(wf.l);
+		wf.diff = wf.w/wf.l;
+
+		fft = initCanvas('render-fft');
+
+		fft.l = wf.l/2;//analyser.frequencyBinCount;
+		fft.min = 1;
+		fft.max = 100;
+		fft.diff = fft.w/(fft.max - fft.min);
+		//fft.array = new Float32Array(fft.l);
+		//analyser.getFloatFrequencyData(fft.array);
+		//console.log(fft.array);
+
+		$("#fftSpan").slider({max:fft.l-1});
+		$("#span-slider").bind('slide',function(evt){
+			var value = $('#fftSpan').slider('getValue');
+			fft.min = value[0];
+			fft.max = value[1];
+			fft.diff = fft.w/(fft.max - fft.min+1);
+		})
+
+		pit = initCanvas('render-pitch');
+
+		//var silence = -100; //This is the FFT output for complete silence, built-in.
+
+		var fftAn = new FFT(SAMPLE_SIZE,44100);
+
+		var processor = context.createScriptProcessor(SAMPLE_SIZE,1,1);
+		processor.onaudioprocess = function(evt){
+			var buff = evt.inputBuffer;
+			var data = buff.getChannelData(0);
+			evt.outputBuffer.getChannelData(0).set(data);
+			window.requestAnimationFrame(function() {
+				wf.array = buff;
+				renderWaveform(data);
+				renderFFT(data,fftAn);
+			});
+		}
 		//Connect audio modules up
 		microphone.connect(gain);
-		gain.connect(analyser);
+		gain.connect(processor);
+		//gain.connect(analyser);
 
 		$("#Oscillator").bind("click", function(evt){
 			if(this.active){
@@ -96,61 +150,15 @@ window.onload = function() {
 		//analyser.connect(context.destination); // Leave mic to speaker playback off by default
 		$("#Feedback").bind("click",function(){
 			if(this.active){
-				analyser.disconnect();
+				//analyser.disconnect();
+				processor.disconnect();
 				this.active=false;
 			} else {
-				analyser.connect(context.destination);
+				//analyser.connect(context.destination);
+				processor.connect(context.destination);
 				this.active=true;
 			}
 		})
-
-
-
-		//For debugging
-		/*console.log(microphone);
-		console.log(analyser);
-		var fbc_array= new Uint8Array(analyser.fftSize); //Not actualy used as an FCB array anymore, but oh well
-		analyser.getByteTimeDomainData(fbc_array);
-		console.log(fbc_array);*/
-
-		//Get some variable ready for the waveform-canvas
-		wf = initCanvas('render-waveform');
-
-		wf.l = 16384;//analyser.fftSize;
-		wf.array = new Uint8Array(wf.l);
-		wf.diff = wf.w/wf.l;
-
-		fft = initCanvas('render-fft');
-
-		fft.l = wf.l/2;//analyser.frequencyBinCount;
-		fft.min = 1;
-		fft.max = 100;
-		fft.diff = fft.w/(fft.max - fft.min);
-		fft.array = new Float32Array(fft.l);
-		analyser.getFloatFrequencyData(fft.array);
-		//console.log(fft.array);
-
-		$("#fftSpan").slider({max:fft.l-1});
-		$("#span-slider").bind('slide',function(evt){
-			var value = $('#fftSpan').slider('getValue');
-			fft.min = value[0];
-			fft.max = value[1];
-			fft.diff = fft.w/(fft.max - fft.min+1);
-		})
-
-		pit = initCanvas('render-pitch');
-
-		//var silence = -100; //This is the FFT output for complete silence, built-in.
-
-		var fftAn = new FFT(16384,44100);
-
-		var processor = context.createScriptProcessor(16384,1,1);
-		processor.onaudioprocess = function(evt){
-			var buff = evt.inputBuffer;
-			evt.outputBuffer = buff;
-			var data = buff.getchannelData(0);
-
-		}
 
 		/*!!function frameLooper(){
 			window.requestAnimationFrame(frameLooper);
@@ -206,31 +214,31 @@ window.onload = function() {
 		frameLooper();*/
 	}
 
-	function renderWaveform(){//(analyser ){
+	function renderWaveform(data){//(analyser ){
 			//analyser.getByteTimeDomainData(wf.array);
 
 			wf.ctx.clearRect(0,0,wf.w,wf.h);
 			wf.ctx.beginPath()
-			wf.ctx.moveTo(0,wf.array[0])
+			wf.ctx.moveTo(0,(data[0]+1)*wf.h/2)
 			for(var j = 0; j < wf.l; j++){
-				wf.ctx.lineTo(j*wf.diff,wf.array[j]);
+				wf.ctx.lineTo(j*wf.diff,(data[j]+1)*wf.h/2);
 			}
 			//wfCtx.closePath();
 			wf.ctx.stroke();
 			// Render maxDiff text
 			wf.ctx.font="10px Georgia";
-			wf.ctx.fillText("maxDiff =" + maxDiff(wf.array,wf.l),10,10);
+			wf.ctx.fillText("maxDiff =" + maxDiff(data,wf.l),10,10);
 	}
 
-	function renderFFT(analyser, fftAn){
+	function renderFFT(data, fftAn){//(analyser, fftAn){
 
 			fft.ctx.clearRect(0,0,fft.w,fft.h);
-			fftAn.forward(wf.array);
+			fftAn.forward(data);
 			var FFTSpec = fftAn.spectrum;
 			fft.ctx.beginPath();
 			for(var i = Math.max(fft.min,0); i <= fft.max && i < fft.l; i++){
 				h = FFTSpec[i];
-				fft.ctx.rect((i-fft.min)*fft.diff, fft.h/2 - (h) ,fft.diff, h);
+				fft.ctx.rect((i-fft.min)*fft.diff, fft.h/2*(1-h) ,fft.diff, h*fft.h/2);
 			}
 			fft.ctx.fill();
 
@@ -247,7 +255,7 @@ window.onload = function() {
 			pit.ctx.clearRect(0,0,pit.w,pit.h);
 			pit.ctx.font="100px Georgia";
 			pit.ctx.textAlign="center";
-			pit.ctx.fillText(top.toFixed(2),pit.w/2,pit.h/2+50);
+			pit.ctx.fillText((top*SAMPLE_RATE/SAMPLE_SIZE).toFixed(2),pit.w/2,pit.h/2+50);
 
 	}
 
@@ -300,8 +308,8 @@ window.onload = function() {
 	{
 		var max = 0;
 		for(var i = 0; i < len; i++){
-			if(Math.abs(128 - array[i]) > max){
-				max = Math.abs(128 - array[i]);
+			if(Math.abs(array[i]) > max){
+				max = Math.abs(array[i]);
 			}
 		}
 		return max;
