bbb80cca7c69ba3f4d3a3e741fd3292521048e8d
Hawkuro <hawkuro@gmail.com>
2014-07-17 16:44:26 +0000

    VocalLine should work now, hypothetically

--- scripts/VocLine.js
+++ scripts/VocLine.js
@@ -2,7 +2,8 @@ function VocalLine(data, noteLength){
 	this.data = data;
 	this.playing = false;
 	this.startTime = null;
-	this.noteLength = noteLength;
+	this.noteLength = 1000*noteLength;
+	this.margins = findMarginIndices(data);
 }
 
 VocalLine.prototype.render = function(ctx){
@@ -17,3 +18,28 @@ VocalLine.prototype.render = function(ctx){
 	ctx.stroke();
 	ctx.restore();
 };
+
+VocalLine.prototype.update = function(){
+	if(true){this.playing = false;}
+};
+
+VocalLine.prototype.play = function(ctx){
+	if(!G.MIDILoaded){return false;}
+	this.playing = true;
+	this.startTime = Date.now()*1000;
+	for(var i = 0; i < this.data.length; i++){
+		if(!data[i]){continue;}
+		if(this.margins.starts[i]){this._startNote(this.data[i].note,i);}
+		if(this.margins.ends[i]){this._endNote(this.data[i].note,i);}
+	}
+};
+
+VocalLine.prototype._startNote = function(Note, index){
+	MIDI.noteOn(0, Note.getNoteNumber(), 1, (this._delay + this.noteLength*index - this.startTime) + 1000*Date.now());
+};
+
+VocalLine.prototype._endNote = function(Note, index){
+	MIDI.noteOn(0, Note.getNoteNumber(), (this._delay + this.noteLength*(index+1) - this.startTime) + 1000*Date.now());
+};
+
+VocalLine.prototype._delay = 0.5;
