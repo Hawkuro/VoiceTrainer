8abcf0b2ac48c7961c12da7c01c6ab4b7d9aeb1f
Hawkuro <hawkuro@gmail.com>
2014-07-18 12:29:18 +0000

    made update function work

--- scripts/VocLine.js
+++ scripts/VocLine.js
@@ -2,28 +2,43 @@ function VocalLine(data, noteLength){
 	this.data = data;
 	this.playing = false;
 	this.startTime = null;
+	this.timeElapsed = 0;
 	this.noteLength = 1000*noteLength;
 	this.margins = findMarginIndices(data);
+	this.timeLength = 0.5 + this.noteLength*this.data.length;
 }
 
 VocalLine.prototype.render = function(ctx){
 	ctx.save();
 	ctx.strokeStyle = "green";
 	ctx.linewidth = 4;
+	var multiplier = this.noteLength*PianoRoll.timeScaler;
 	for(var i = 0; i < this.data.length; i++){
 		if(!this.data[i]){continue;}
 		var h = noteYPos(this.data[i].note, PianoRoll.lineDiff, PianoRoll.canv.h);
-		plotLine(noteLength*i, h, noteLength*(i+1), h);
+		plotLine(multiplier*i, h, multiplier*(i+1), h);
 	}
 	ctx.stroke();
 	ctx.restore();
 };
 
 VocalLine.prototype.update = function(){
-	if(true){this.playing = false;}
+	if(this.playing){
+	this.timeElapsed = Date.now()*1000 - this.startTime;
+		if(this.timeElapsed > this.timeLength){
+			this.stop();
+		}
+	}
 };
 
-VocalLine.prototype.play = function(ctx){
+VocalLine.prototype.stop = function(){
+	MIDI.Player.stopAllNotes();
+	this.playing = false;
+	this.startTime = undefined;
+	this.timeElapsed = 0;
+}
+
+VocalLine.prototype.play = function(){
 	if(!G.MIDILoaded){return false;}
 	this.playing = true;
 	this.startTime = Date.now()*1000;
